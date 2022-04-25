1c39234e251aa116e5bfaf430933125c217af3ab
rahymzhanalibekov <rahymzhan@gmail.com>
2022-04-25 17:33:58 +0600

    25042022_1

--- src/home_work/ru/geekbrains/java_two/lesson_a/Background.java
+++ src/home_work/ru/geekbrains/java_two/lesson_a/Background.java
@@ -7,7 +7,7 @@ public class Background{
     private static Random rnd = new Random();
     private Color color;
     Background(GameCanvas canvas){
-        if (canvas.background_change_interval >= 15) {
+        if (canvas.background_change_interval >= 16) {
             color = new Color(rnd.nextInt());
             canvas.setBackground(color);
             canvas.background_change_interval = 0;
